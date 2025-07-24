49bef1abd1ef658c625b55379b8c2f28d9d2249b
jestruvec <jhonnyestruve@gmail.com>
2025-07-24 17:57:57 -0600

    fix(i18n): deleted old basename

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -14,7 +14,7 @@ i18n
       escapeValue: false,
     },
     backend: {
-      loadPath: "/tesseract/locales/{{lng}}/translation.json",
+      loadPath: "/locales/{{lng}}/translation.json",
     },
   });
 
